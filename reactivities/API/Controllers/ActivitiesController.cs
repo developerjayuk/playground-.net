83f3b12b639e2343502f2c40a42fbbd6a0a21f08
jasonj85 <jasonj85@gmail.com>
2020-12-30 11:27:54 +0000

    Basic crud func done for api

--- reactivities/API/Controllers/ActivitiesController.cs
+++ reactivities/API/Controllers/ActivitiesController.cs
@@ -4,7 +4,6 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -29,6 +28,25 @@ namespace API.Controllers
     public async Task<ActionResult<Activity>> Details(Guid id)
     {
       return await _mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Unit>> Create(Create.Command command)
+    {
+      return await _mediator.Send(command);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+    {
+      command.Id = id;
+      return await _mediator.Send(command);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Unit>> Delete(Guid id)
+    {
+      return await _mediator.Send(new Delete.Command { Id = id });
     }
   }
 }
--- reactivities/Application/Activities/List.cs
+++ reactivities/Application/Activities/List.cs
@@ -1,11 +1,8 @@
 ﻿using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
