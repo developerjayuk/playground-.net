d0b5fa585fcf2c2a11db19f6d10d75680acd04b3
jasonj85 <jasonj85@gmail.com>
2021-03-26 07:14:25 +0000

    Testing some basics and features

---
+++ helloWorld/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+//#error version
+
+namespace helloWorld
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Hello C#!");
+      Console.WriteLine();
+
+      try
+      {
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+        throw;
+      }
+    }
+  }
+
+  internal class MyClass
+  {
+    private string _test;
+
+    public MyClass(string test)
+    {
+      _test = test;
+    }
+  }
+}
