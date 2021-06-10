13172541e839e036091b3aff236527fb51b3858d
jasonj85 <jasonj85@gmail.com>
2021-06-10 08:01:43 +0100

    Setup and creation of basic skeleton for new social network activities project

---
+++ entityFramework/1st-demo/MyFirstEfCoreApp/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstEfCoreApp
+{
+  public class AppDbContext : DbContext
+  {
+    private const string ConnectionString =            //#A
+        @"Server=(localdb)\mssqllocaldb;
+             Database=MyFirstEfCoreDb;
+             Trusted_Connection=True";
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer(ConnectionString); //#B
+    }
+
+    public DbSet<Book> Books { get; set; }
+  }
+
+  /********************************************************
+  #A The connection string is used by the SQL Server database provider to find the database
+  #B Using the SQL Server database provider’s UseSqlServer command sets up the options ready for creating the applications’s DBContext
+   ********************************************************/
+}
---
+++ entityFramework/1st-demo/MyFirstEfCoreApp/Author.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFirstEfCoreApp
+{
+  public class Author
+  {
+    public int AuthorId { get; set; }    //#D
+    public string Name { get; set; }
+    public string WebUrl { get; set; }
+  }
+
+  /*******************************************************
+  #D This holds the Primary Key of the Author row in the database
+   * ************************************************/
+}
