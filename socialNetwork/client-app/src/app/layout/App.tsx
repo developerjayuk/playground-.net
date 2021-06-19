0e9d72a192cef439584bea2873561f71f3bbf5e5
jasonj85 <jasonj85@gmail.com>
2021-06-19 11:20:18 +0100

    Add images and sort folders better

---
+++ socialNetwork/client-app/src/app/layout/App.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((res) => {
+        setActivities(res.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          <ul>
+            {activities.map((activity) => (
+              <List.Item key={activity.id}>{activity.title}</List.Item>
+            ))}
+          </ul>
+        </List>
+      </Container>
+    </>
+  );
+}
+
+export default App;
