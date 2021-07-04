ea44b078c779c7424870a472e5e41265b4adc020
jasonj85 <jasonj85@gmail.com>
2021-07-04 09:27:04 +0100

    sorting out rest of axios commands

--- socialNetwork/client-app/src/app/api/agent.ts
+++ socialNetwork/client-app/src/app/api/agent.ts
@@ -1,7 +1,24 @@
 import axios, { AxiosResponse } from "axios";
+import { request } from "http";
 import { Activity } from "../models/activity";
 
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
 axios.defaults.baseURL = "http://localhost:5000/api";
+
+axios.interceptors.response.use(async (response) => {
+  try {
+    await sleep(1000);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return await Promise.reject(error);
+  }
+});
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
@@ -15,6 +32,11 @@ const requests = {
 
 const Activities = {
   list: () => requests.get<Activity[]>("/activities"),
+  details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+  create: (activity: Activity) => requests.post<void>("/activities", activity),
+  update: (activity: Activity) =>
+    requests.put<void>(`/activities/${activity.id}`, activity),
+  delete: (id: string) => requests.delete<void>(`/activities/${id}`),
 };
 
 const agent = {
