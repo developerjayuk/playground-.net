262f7b72725b15bc1bddd8002f5481426b3eff03
INT\19031238 <jason.james@csdental.com>
2021-07-20 13:03:33 +0100

    additional error handling changes, add toastify

--- socialNetwork/client-app/src/app/layout/App.tsx
+++ socialNetwork/client-app/src/app/layout/App.tsx
@@ -4,14 +4,18 @@ import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import HomePage from "../../features/home/HomePage";
 import { observer } from "mobx-react-lite";
-import { Route, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestErrors";
+import NotFound from "../../features/errors/NotFound";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const location = useLocation();
   return (
     <>
+      <ToastContainer position="bottom-right" hideProgressBar />
       <Route exact path="/" component={HomePage} />
       <Route
         path={"/(.+)"}
@@ -19,13 +23,17 @@ function App() {
           <>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route exact path="/activities" component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                path={["/createActivity", "/manage/:id"]}
-                component={ActivityForm}
-                key={location.key}
-              />
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                  key={location.key}
+                />
+                <Route path="/errors" component={TestErrors} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
--- socialNetwork/client-app/src/app/layout/NavBar.tsx
+++ socialNetwork/client-app/src/app/layout/NavBar.tsx
@@ -15,6 +15,8 @@ export default function NavBar() {
           Social Network
         </Menu.Item>
         <Menu.Item name="Activities" as={NavLink} to="/activities" />
+        <Menu.Item name="Errors" as={NavLink} to="/errors" />
+
         <Menu.Item>
           <Button
             positive
