03813781ff44ba8d40499110213383a32ad16a50
jasonj85 <jasonj85@gmail.com>
2021-10-27 21:48:08 +0100

    updated seed data and isHostRequirements

--- socialNetwork/Infrastructure/Security/IsHostRequirement.cs
+++ socialNetwork/Infrastructure/Security/IsHostRequirement.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure.Security
@@ -15,7 +18,8 @@ namespace Infrastructure.Security
     {
         private readonly DataContext _dbContext;
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public IsHostRequirementHandler(DataContext dbContext, IHttpContextAccessor httpContextAccessor)
+        public IsHostRequirementHandler(DataContext dbContext,
+            IHttpContextAccessor httpContextAccessor)
         {
             this._httpContextAccessor = httpContextAccessor;
             this._dbContext = dbContext;
@@ -24,6 +28,22 @@ namespace Infrastructure.Security
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
         {
             var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId == null) return Task.CompletedTask;
+
+            var activityId = Guid.Parse(_httpContextAccessor.HttpContext?.Request.RouteValues
+            .SingleOrDefault(x => x.Key == "id").Value?.ToString());
+
+            var attendee = _dbContext.ActivityAttendees
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.AppUserId == userId && x.ActivityId == activityId)
+                .Result;
+
+            if (attendee == null) return Task.CompletedTask;
+
+            if (attendee.IsHost) context.Succeed(requirement);
+
+            return Task.CompletedTask;
         }
     }
 }
