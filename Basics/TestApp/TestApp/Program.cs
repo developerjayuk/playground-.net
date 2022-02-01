e2f2f976a44be5ff6e33931180b3fc6334b158b6
jasonj85 <jasonj85@gmail.com>
2022-02-01 18:03:15 +0000

    organising folders better

---
+++ Basics/TestApp/TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Welcome the user to the app
+Console.WriteLine("Hello there stranger and welcome to this application!");
+Console.WriteLine("-----------------------------------\n");
+
+// Ask for the firstname
+var firstName = "";
+
+while (firstName == "")
+{
+    Console.Write("What is your name friend?: ");
+    firstName = Console.ReadLine();
+}
+
+// Greet user by name
+Console.WriteLine($"Hi {firstName}, let's get to know each other !");
