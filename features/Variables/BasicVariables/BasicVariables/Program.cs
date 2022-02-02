943f70a32ef4aa624db9c13f07a9129e3d4f17cb
jasonj85 <jasonj85@gmail.com>
2022-02-02 12:07:20 +0000

    minor updates

--- features/Variables/BasicVariables/BasicVariables/Program.cs
+++ features/Variables/BasicVariables/BasicVariables/Program.cs
@@ -41,11 +41,41 @@ bool? human = null;
 double? shootingPercentage = null;
 string? middleName = null;
 
-void GetPersonDetails()
+// type conversions
+//Console.WriteLine("What is your age?");
+//bool validAge = int.TryParse(Console.ReadLine(), out int personAge);
+
+//Console.WriteLine($"Age is {personAge}");
+
+Assignment2();
+
+void Assignment1()
 {
     string firstName = "Bob";
     string lastName = "Jones";
     int age = 32;
     bool isAlive = true;
     string telephoneNumber = "+44 4958938493";
-} 
+}
+
+void Assignment2()
+{
+    bool validAge = false;
+    int age = 0;
+
+    while (!validAge)
+    {
+        Console.WriteLine("What is your age?");
+        validAge = int.TryParse(Console.ReadLine(), out age);
+    }
+
+    if (age > 0)
+    {
+        var futureAge = age + 25;
+        var pastAge = age > 25 ? age - 25 : 0;
+
+        Console.WriteLine($"You are now {age} years old");
+        Console.WriteLine($"In 25 years you will be {futureAge}");
+        Console.WriteLine($"25 years age you were {pastAge}");
+    }
+}
