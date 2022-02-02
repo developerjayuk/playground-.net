4a686a145484a5083415f33116c7b0232f4862c3
jasonj85 <jasonj85@gmail.com>
2022-02-02 12:49:34 +0000

    basics recap

---
+++ features/Conditionals/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,75 @@
+﻿//BasicIf();
+
+//MoreAdvancedIf();
+
+Switch();
+
+
+void Basic()
+{
+    bool isComplete = false;
+
+    if (isComplete)
+    {
+        Console.WriteLine($"The statement is {isComplete}");
+    }
+    else
+    {
+        Console.WriteLine($"The statement is {isComplete}");
+    }
+
+    Console.WriteLine("Please enter your dog's name?");
+    var dogName = Console.ReadLine();
+
+    if (dogName.ToLower() == "jerry")
+    {
+        Console.WriteLine("Welcome we have confirmed you are the correct owner!!");
+    }
+}
+
+void MoreAdvancedIf()
+{
+    Console.Write("What is your first name: ");
+    string firstName = Console.ReadLine();
+
+    Console.Write("What is your last name: ");
+    string lastName = Console.ReadLine();
+
+    if (firstName.ToLower() == "jimbo" && lastName.ToLower() == "jones")
+    {
+        Console.WriteLine("Welcome Mr Admin!");
+    }
+    else if (lastName.ToLower() == "jones")
+    {
+        Console.WriteLine("are you related to the Admin? y/n");
+        bool related = Console.ReadLine() == "y" ? true : false;
+
+        if (!related)
+        {
+            Console.WriteLine("Sorry only family members are allowed access to this app.");
+        }
+    }
+}
+
+void Switch()
+{
+    var firstName = "Jason";
+    var greeting = "";
+
+    switch (firstName.ToLower())
+    {
+        case "jason":
+            greeting = "admin";
+            break;
+        case "bob":
+            greeting = "moderator";
+            break;
+        default:
+            greeting = "basic";
+            break;
+    }
+
+    Console.WriteLine($"Welcome {greeting} user");
+}
+
+Console.WriteLine("this is the end of the app");
---
+++ features/DateTime/DateTime/DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+DateTime todayLocal = DateTime.Now; // local time
+DateTime todayGlobal = DateTime.UtcNow; // global time
+
+DateTime birthdayJoe = DateTime.Parse("21-01-1960");
+DateTime birthdayDave = DateTime.ParseExact("08/02/1951", "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+Console.WriteLine(todayLocal);
+Console.WriteLine(todayLocal.ToString("d"));
+Console.WriteLine(todayLocal.ToShortTimeString());
+
+Console.WriteLine(birthdayJoe.ToShortDateString());
+
+// DateOnly
+DateOnly birthdayCarry = DateOnly.Parse(DateTime.Now.AddYears(-30).ToShortDateString());
+Console.WriteLine(birthdayCarry);
+
+// TimeOnly
+TimeOnly timeHappened = TimeOnly.FromDateTime(DateTime.Now);
+Console.WriteLine(timeHappened);
