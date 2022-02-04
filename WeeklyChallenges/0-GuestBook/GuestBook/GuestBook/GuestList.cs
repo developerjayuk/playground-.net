630e8e5d2d6b8f3091070789f4587ee37695add4
jasonj85 <jasonj85@gmail.com>
2022-02-04 14:43:56 +0000

    completed Guestbook console app challenge

---
+++ WeeklyChallenges/0-GuestBook/GuestBook/GuestBook/GuestList.cs
@@ -0,0 +1,122 @@
+﻿namespace GuestBook
+{
+    internal class GuestList
+    {
+        private readonly Dictionary<int, string> _menuOptions = new()
+        {
+            { 1, "Add guest to guest list" },
+            { 2, "Show the guest list" },
+            { 0, "Exit the program" }
+        };
+
+        private Dictionary<string, int> FullList = new();
+        private int FullPartySize = 0;
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine($"----------------------------------------------------------");
+            Console.WriteLine($"\nPlease select from one of the {_menuOptions.Count} menu options below:");
+
+            foreach (var option in _menuOptions)
+            {
+                Console.WriteLine($"{option.Key} - {option.Value}");
+            }
+
+            var input = Console.ReadLine();
+            ConfirmMenuSelection(input);
+        }
+        public void ConfirmMenuSelection(string? input)
+        {
+            var valid = false;
+            valid = int.TryParse(input, out var option) && _menuOptions.ContainsKey(option);
+            
+            if (valid)
+            {
+                switch (option)
+                {
+                    case 1:
+                        AddNewGuest();
+                        break;
+                    case 2:
+                        DisplayFullGuestList();
+                        break;
+                    case 3:
+                        ExitApplication();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("** Invalid selection please try again **");
+                DisplayMenu();
+            }
+        }
+
+        private void AddNewGuest()
+        {
+            var name = "";
+            var partySize = 0;
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(" * Add New Guest");
+
+            do
+            {
+                Console.Write("Please enter your name: ");
+                name = Console.ReadLine();
+
+            } while (!ValidName(name));
+
+            do
+            {
+                Console.WriteLine("Please enter the size of your party as a number: ");
+                Console.WriteLine("- maximum size is 20");
+                var partySizeInput = Console.ReadLine();
+
+                int.TryParse(partySizeInput, out partySize);
+
+            } while (!ValidPartySize(partySize));
+
+            FullList.Add(name, partySize);
+            FullPartySize += partySize;
+            Console.WriteLine("** User successfully added to the guest list");
+            
+            DisplayMenu();
+        }
+
+        private void DisplayFullGuestList()
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($" Total number of groups: {FullList.Count}");
+            Console.WriteLine($" Total number of guests: {FullPartySize}");
+
+            if (FullList.Count > 0)
+            {
+                Console.WriteLine("\nFull Guest List is below:");
+
+                foreach (var guest in FullList)
+                {
+                    Console.WriteLine($" - Guest {guest.Key} as a party of {guest.Value}.");
+                }
+            }
+
+            DisplayMenu();
+        }
+
+        private void ExitApplication()
+        {
+            Console.WriteLine("Thank you for using our application. Have a good day :)");
+            Environment.Exit(0);
+        }
+
+        private bool ValidName(string input)
+        {
+            return input.Length > 0;
+        }
+        private bool ValidPartySize(int input)
+        {
+            return input is > 0 and <= 20;
+        }
+
+    }
+}
---
+++ WeeklyChallenges/0-GuestBook/GuestBook/GuestBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using GuestBook;
+
+// REQUIREMENTS
+//1.Welcome user to the app
+//2. Have a menu 
+//a. add to guestlist
+//b. show guestlist
+//3. Ask for their full name 
+//    - confirm valid string
+//4. Ask how many people are in their party
+//    - confirm a valid number
+//5. Keep track of how many people are at the party
+//6. Print out the guestlist
+//7. Print out the total number of guest
+
+GuestList guestList = new();
+
+Console.WriteLine("Hello and welcome to the hottest party of the Year \"The C# Pool Party\"");
+guestList.DisplayMenu();
