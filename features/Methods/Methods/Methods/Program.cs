f026ed634ce9f8f0c32b1c135a0a238b249d08f5
jasonj85 <jasonj85@gmail.com>
2022-02-04 13:01:02 +0000

    methods recap

---
+++ features/Methods/Methods/Methods/Program.cs
@@ -0,0 +1,9 @@
+﻿ using Methods;
+
+//var fullName = SampleMethods.GetFullName();
+//var (firstName, lastName) = fullName;
+//var (firstName, _) = fullName; - use discard character
+
+SampleMethods.ShowWelcomeMessage();
+var userName = SampleMethods.GetUserName();
+SampleMethods.SayHi(userName);
---
+++ features/Methods/Methods/Methods/SampleMethods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public static class SampleMethods
+    {   
+        // tuple 
+        public static (string firstName, string lastName) GetFullName()
+        {
+            Console.Write("Please enter your first name: ");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Please enter your last name: ");
+            var lastName = Console.ReadLine();
+
+            return (firstName, lastName);
+        }
+
+        public static string GetUserName()
+        {
+            Console.Write("Please enter your user name: ");
+            return Console.ReadLine();
+        }
+
+        public static void ShowWelcomeMessage()
+        {
+            Console.WriteLine($"Welcome to my app, let's get started....");
+        }
+
+        public static void SayHi(string name = "User")
+        {
+            Console.WriteLine($"Hi {name}!");
+        }
+
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye... thanks for using the application :)");
+        }
+    }
+}
