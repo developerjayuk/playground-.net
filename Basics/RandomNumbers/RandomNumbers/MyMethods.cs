340b89ece1641cc9efb4f6b8dbb430fe11a5a022
jasonj85 <jasonj85@gmail.com>
2022-02-05 17:59:29 +0000

    testing out some random numbers

---
+++ Basics/RandomNumbers/RandomNumbers/MyMethods.cs
@@ -0,0 +1,16 @@
+﻿namespace RandomNumbers
+{
+    class MyMethods
+    {
+        public static void PrintRandomNumber(Random random)
+        {
+            Console.WriteLine($"Your random int is: {random.Next(1, 11)}");
+            Console.WriteLine($"Your random double is: {random.NextDouble() * 10}");
+        }
+
+        public static IList<T> ShuffleList<T>(IList<T> list, Random random)
+        {
+            return list.OrderBy(n => random.Next()).ToList();
+        }
+    }
+}
---
+++ Basics/RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using RandomNumbers;
+
+Random random = new();
+
+for (int i = 0; i < 10; i++)
+{
+    MyMethods.PrintRandomNumber(random);
+}
+
+List<string> guestList = new()
+{
+    "Abigail",
+    "Max",
+    "Steve",
+    "Zeus"
+};
+
+var shuffledList = MyMethods.ShuffleList(guestList, random);
+
+foreach (var guest in shuffledList)
+{
+    Console.WriteLine(guest);
+}
