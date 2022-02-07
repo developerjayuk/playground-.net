2749a5b1bbbc90dd9b313263cbba71eb16931585
jasonj85 <jasonj85@gmail.com>
2022-02-07 09:57:48 +0000

    recap of property types

---
+++ features/PropertyTypes/PropertyTypes/PropertyTypes/PersonModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace PropertyTypes
+{
+    internal class PersonModel
+    {
+        public PersonModel()
+        {
+            RegistrationDate = DateTime.Now;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        public DateTime RegistrationDate { get; set; }
+
+        private int _age;
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value is >= 0 and < 130)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Age out of range");
+                }
+            }
+        }
+
+        private string _ssn;
+
+        public string SSN
+        {
+            get
+            {
+                string output = "***-**-" + _ssn.Substring(_ssn.Length - 4);
+                return output;
+            }
+            set => _ssn = value;
+        }
+    }
+}
---
+++ features/PropertyTypes/PropertyTypes/PropertyTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PropertyTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new()
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                Age = 78,
+                SSN = "123-45-6789"
+            };
+
+            Console.WriteLine($"{person.FullName} ({person.SSN})");
+
+            Console.ReadLine();
+        }
+    }
+}
