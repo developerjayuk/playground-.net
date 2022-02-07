8c383f012396077bedcb187a111f04256e12ed86
jasonj85 <jasonj85@gmail.com>
2022-02-07 12:06:20 +0000

    instantiated classes demo

---
+++ features/InstantiatedClasses/InstantiatedClasses/InstantiatedClasses/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿namespace InstantiatedClasses
+{
+    public class PersonModel
+    {
+        public PersonModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string EmailAddress => $"{FirstName}@gmail.com";
+    }
+}
---
+++ features/InstantiatedClasses/InstantiatedClasses/InstantiatedClasses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InstantiatedClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new()
+            {
+                new PersonModel("Tom", "Jones"),
+                new PersonModel("Bob", "Smith"),
+                new PersonModel("Jane", "Foome"),
+            };
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Firstname: {person.FirstName}; Lastname: {person.LastName}; Email: {person.EmailAddress}");   
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
