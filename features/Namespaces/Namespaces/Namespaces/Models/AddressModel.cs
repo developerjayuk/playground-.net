b99ec489f83dd2367a715dd61d8485025e72c96f
jasonj85 <jasonj85@gmail.com>
2022-02-07 12:06:31 +0000

    namespaces demo

---
+++ features/Namespaces/Namespaces/Namespaces/Models/AddressModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Namespaces.Models
+{
+    public class AddressModel
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ features/Namespaces/Namespaces/Namespaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Namespaces.Models;
+
+namespace Namespaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new();
+
+            Calculations.Add(4.5, 8.4);
+
+            Console.ReadLine();
+        }
+    }
+}
