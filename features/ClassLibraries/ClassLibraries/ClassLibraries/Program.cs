aea3f254c898f81c9eece33b99da703813b7982c
jasonj85 <jasonj85@gmail.com>
2022-02-07 12:05:44 +0000

    class library demo

---
+++ features/ClassLibraries/ClassLibraries/ClassLibraries/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DemoLibrary;
+using DemoLibrary.Models;
+
+namespace ClassLibraries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new();
+
+            Calculations.Add(3.4, 10);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ features/ClassLibraries/ClassLibraries/DemoLibrary/Calculations.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoLibrary
+{
+    public static class Calculations
+    {
+        public static double Add(double x, double y)
+        {
+            double output = 0;
+
+            output = x + y;
+
+            return output;
+        }
+    }
+}
