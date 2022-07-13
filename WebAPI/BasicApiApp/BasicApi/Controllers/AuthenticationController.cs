cea7a4e2d5b9e823c06f8259d5468a1e9a416473
jasonj85 <jasonj85@gmail.com>
2022-07-13 15:51:55 +0100

    Add basic example of authentication using JWT bearer token

---
+++ WebAPI/BasicApiApp/BasicApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BasicApi.Controllers;
+
+/// <summary>
+/// Basic example, not real production ready code
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    public IConfiguration _config { get; }
+    public record AuthenticationData(string? UserName, string? Password);
+    public record UserData(int UserId, string UserName, string Title, string EmployeeId);
+
+    public AuthenticationController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    // api/authentication/token
+    [HttpPost("token")]
+    public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+    {
+        var user = ValidateCredentials(data);
+
+        if (user is null)
+        {
+            return Unauthorized();
+        }
+
+        var token = GenerateToken(user);
+
+        return Ok(token);
+    }
+
+    private string GenerateToken(UserData user)
+    {
+        var secretKey =
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                _config.GetValue<string>("Authentication:SecretKey")));
+
+        var signingCredentials =
+            new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claims = new();
+        claims.Add(new(JwtRegisteredClaimNames.Sub, user.UserId.ToString()));
+        claims.Add(new(JwtRegisteredClaimNames.UniqueName, user.UserName));
+        claims.Add(new("title", user.Title));
+        claims.Add(new("employeeId", user.EmployeeId));
+
+
+        var token = new JwtSecurityToken(
+            _config.GetValue<string>("Authentication:Issuer"),
+            _config.GetValue<string>("Authentication:Audience"),
+            claims,
+            DateTime.UtcNow, // time token is valid
+            DateTime.UtcNow.AddMinutes(2), // time token expires
+            signingCredentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserData? ValidateCredentials(AuthenticationData data)
+    {
+        if (CompareValues(data.UserName, "homer") &&
+            CompareValues(data.Password, "homer123"))
+        {
+            return new UserData(1, data.UserName!, "CEO", "001");
+        }
+
+        if (CompareValues(data.UserName, "bart") &&
+            CompareValues(data.Password, "bart123"))
+        {
+            return new UserData(2, data.UserName!, "Tech Lead", "002");
+        }
+
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string expected)
+    {
+        if (actual is not null)
+        {
+            if (actual.Equals(expected))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
---
+++ WebAPI/BasicApiApp/BasicApi/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    public IConfiguration _config { get; }
+
+    public UsersController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    // GET: api/users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        List<string> output = new();
+        for (int i = 0; i < Random.Shared.Next(2,10); i++)
+        {
+            output.Add($"Value #{i+1}");
+        }
+
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/users/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return $"Value #{id}";
+    }
+
+    // POST api/users
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/users/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/users/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
