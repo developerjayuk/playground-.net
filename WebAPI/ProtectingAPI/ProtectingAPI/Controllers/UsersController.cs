bc4f06a214d6125a1284d823b7a05b8331566338
jasonj85 <jasonj85@gmail.com>
2022-07-16 13:09:26 +0100

    added validation using data annotations

---
+++ WebAPI/ProtectingAPI/ProtectingAPI/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProtectingAPI.Models;
+
+namespace ProtectingAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    // GET: api/<UsersController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public IActionResult Post([FromBody] UserModel user)
+    {
+        if (ModelState.IsValid)
+        {
+            return Ok("The model was valid");
+        }
+        else
+        {
+            return BadRequest(ModelState);
+        }
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
---
+++ WebAPI/ProtectingAPI/ProtectingAPI/Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProtectingAPI.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(10)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(10)]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Url]
+        public string Portfolio { get; set; }
+
+        [Range(0, 5)]
+        public int NumberOfReferences { get; set; }
+    }
+}
