bd935c6b0bc9b3955c867626f48eee9bfa204213
jasonj85 <jasonj85@gmail.com>
2022-07-19 08:45:53 +0100

    Add dataAccess code and interfaces

--- WebAPI/TodoApp/TodoAPI/Program.cs
+++ WebAPI/TodoApp/TodoAPI/Program.cs
@@ -1,34 +1,11 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using TodoAPI.StartupConfig;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAuthorization(opts =>
-{
-    opts.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-});
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer(opts =>
-    {
-        opts.TokenValidationParameters = new()
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
-            ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
-                .GetBytes(builder.Configuration.GetValue<string>("Authentication:SecretKey")))
-        };
-    });
-builder.Services.AddHealthChecks()
-    .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+builder.AddStandardServices();
+builder.AddAuthServices();
+builder.AddHealthCheckServices();
+builder.AddCustomServices();
 
 var app = builder.Build();
 
