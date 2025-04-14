d7535922bffbf21c34359cc0ba2ea0de7068eb6e
Jason James <jason.james@cba-japan.com>
2025-04-14 21:17:54 +0100

    add initial unit tests

---
+++ WorldTravel/WorldTravel.Application.Tests/Users/CurrentUserTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using WorldTravel.Application.Users;
+using WorldTravel.Domain.Constants;
+using Xunit;
+
+namespace WorldTravel.Application.Tests.Users;
+
+public class CurrentUserTests
+{
+    // naming convention for test methods:  
+    // [MethodName_Scenario_ExpectedResult]  
+    [Fact()]
+    public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+    {
+        // Arrange  
+        var currentUser = new CurrentUser("1", "test@test.com", [ UserRoles.User, UserRoles.Admin ], null);
+
+        // Act  
+        var result = currentUser.IsInRole(UserRoles.Admin);
+
+        // Assert  
+        result.Should().BeTrue();
+    }
+
+    [Fact()]
+    public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
+    {
+        // Arrange  
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.User, UserRoles.Admin], null);
+
+        // Act  
+        var result = currentUser.IsInRole(UserRoles.Owner);
+
+        // Assert  
+        result.Should().BeFalse();
+    }
+}
